f13e19e114e56fc8dc2f302fcddef54cc5e8d72c
Mario Thompson <MarioThompson0010@gmail.com>
2021-03-07 20:24:27 -0600

    user created

--- server/models/user.js
+++ server/models/user.js
@@ -16,7 +16,7 @@ module.exports  = function  (sequelize, DataTypes) {
         type: DataTypes.STRING,
         allowNull: false
       },
-      usernameField: {
+      username: {
         type: DataTypes.STRING,
         allowNull: true,
         unique: true,
